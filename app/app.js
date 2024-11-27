b8d4cd276ddc6c4ed995bf970e91f2e8b8412e9a
Xulio <a22julioaa@iessanclemente.net>
2024-11-27 11:03:05 +0100

    feature(nav): fix navbar a bottom

--- app/app.js
+++ app/app.js
@@ -3,16 +3,29 @@ let horizontalSection = document.querySelector('.horizontal')
 
 console.log(horizontalSection.scrollWidth)
 
-gsap.to('.horizontal', {
-    x: () => horizontalSection.scrollWidth * -1,
-    xPercent: 100,
-    scrollTrigger: {
-        trigger: '.horizontal',
-        start: 'center center',
-        end: '+=3000px',
-        pin: '.container',
-        scrub: true,
-        markers: true,
-        invalidateOnRefresh: true,
+// gsap.to('.horizontal', {
+//     x: () => horizontalSection.scrollWidth * -1,
+//     xPercent: 100,
+//     scrollTrigger: {
+//         trigger: '.horizontal',
+//         start: 'center center',
+//         end: '+=3000px',
+//         pin: '.container',
+//         scrub: true,
+//         markers: true,
+//         invalidateOnRefresh: true,
+//     }
+// })
+
+const menuNavegacion = document.getElementById('menu-navegacion')
+
+window.addEventListener('scroll', () => {
+    const scrollPosition  = window.scrollY;
+
+    if(scrollPosition >= window.innerHeight) {
+        menuNavegacion.classList.add('fixed')
+    } else {
+        menuNavegacion.classList.remove('fixed')
     }
-})
+}
+);
